4f53fba7c8100e161034bbf9733a1df557aa80d0
hakyuwon <kyu10225@naver.com>
2025-07-21 02:42:14 +0900

    Back up

--- Backend/src/main/java/me/hakyuwon/sweetCheck/controller/UserController.java
+++ Backend/src/main/java/me/hakyuwon/sweetCheck/controller/UserController.java
@@ -4,21 +4,29 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.firebase.auth.FirebaseAuthException;
 import lombok.RequiredArgsConstructor;
-import me.hakyuwon.sweetCheck.dto.LoginResponse;
-import me.hakyuwon.sweetCheck.dto.TokenRequest;
+import me.hakyuwon.sweetCheck.dto.*;
+import me.hakyuwon.sweetCheck.service.MealService;
+import me.hakyuwon.sweetCheck.service.UserService;
+import me.hakyuwon.sweetCheck.util.SecurityUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Collections;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-    // ✅ 안드로이드에서 사용하는 Web Client ID를 여기에!
+    // 안드로이드에서 사용하는 Web Client ID
     private static final String CLIENT_ID = "983762013559-1uq109l17mvci4peipqo0ua1stf3dd3t.apps.googleusercontent.com";
+    private final UserService userService;
+    private final MealService mealService;
 
     @PostMapping("/api/users/login")
     public ResponseEntity<LoginResponse> login(@RequestBody TokenRequest tokenRequest) {
@@ -56,5 +64,39 @@ public class UserController {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @ResponseBody
+    @GetMapping("/api/home")
+    public DailyMealResponse home(@AuthenticationPrincipal UserDetails userDetails) {
+        String uid = userDetails.getUsername();
+        DailyMealResponse dailyMealResponse = new DailyMealResponse();
+        dailyMealResponse = mealService.getDailyMeals(uid, LocalDate.now());
+        return dailyMealResponse ;
+    }
+
+
+    @PostMapping("/api/users/profile")
+    public ResponseEntity<String> updateProfile(@RequestBody ProfileRequest profileRequest) {
+        userService.saveUserProfile(profileRequest);
+        return ResponseEntity.ok("Profile registered successfully");
+    }
+
+    @DeleteMapping("/api/users/{uid}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String uid) {
+        try {
+            userService.deleteUser(uid);
+            return ResponseEntity.noContent().build();
+        } catch (FirebaseAuthException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // weekly report api
+    @GetMapping("/api/meals/stats")
+    public ResponseEntity<WeeklyReportResponse> getWeeklyStats() {
+        String userId = SecurityUtil.getCurrentUserId();
+        WeeklyReportResponse response = userService.getWeeklySugarStats(userId);
+        return ResponseEntity.ok(response);
     }
 }
